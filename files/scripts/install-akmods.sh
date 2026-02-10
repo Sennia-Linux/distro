78608ffff4033d28577777594eab5de49461523d
PolyCat <contact.polycat@proton.me>
2026-02-10 14:40:43 +0200

    Moved install-akmods script into recipe as snippets

--- files/scripts/install-akmods.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-find /tmp/rpms
-dnf5 install -y /tmp/rpms/kmods/kmod-v4l2loopback*.rpm

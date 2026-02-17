fb3599a3539734780df6c496ec2ecdb60f70e066
PolyCat <contact.polycat@proton.me>
2026-02-17 12:16:15 +0200

    Made install-plymouth-theme.sh delete git repo before install

--- files/scripts/install-plymouth-theme.sh
+++ files/scripts/install-plymouth-theme.sh
@@ -5,6 +5,7 @@ set -ueo pipefail
 dnf5 install -y plymouth-plugin-script
 
 git clone https://github.com/Sienna-Linux/plymouth-theme.git running-cat
+rm -rf running-cat/.git
 mv running-cat /usr/share/plymouth/themes/
 
 plymouth-set-default-theme running-cat
